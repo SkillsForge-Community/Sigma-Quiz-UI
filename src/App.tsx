9674bedebd0dc59fe3c89baa6068b5898a0512c0
Silas <silasadegoke331@gmail.com>
2024-06-04 11:33:24 +0100

    updated the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Homepage from './components/Homepage';
+import Homepage from './components/Homepage/Homepage';
 import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
       <Homepage/>
+      
     </div>
   );
 }
---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,54 @@
+import logo from "./Images/logo.png"
+import infor from "./Images/info.png"
+import { IoPlayCircleOutline } from "react-icons/io5"
+import { MdKeyboardArrowDown } from "react-icons/md";
+import "./Homepage.css"
+const Homepage = () => {
+    return (
+        <div className="Home">
+            <div className="logos">
+                <img src={logo} className="logo" alt="logo" />
+                <div>
+                    <h4 className="logo-text">Sigma Club</h4>
+                    <h5 className="logo-text">Since 1950</h5>
+                </div>
+            </div>
+            <div className="title">
+                <h1>Sigma Roseline Etuokwu Quiz Competition</h1>
+                <h3>Quiz competition involving secondary schools from all over Nigeria. Instilling the spirit of knowledge. Held @ the premier University; University of Ibadan</h3>
+            </div>
+            <div className="dropdown">
+                <button className="dropdown-menu">
+                    <h5>Select quiz to view</h5>
+                    <MdKeyboardArrowDown />
+                </button>
+                <button className="submit" type="submit">View Quiz</button>
+            </div>
+            <div className="quizes">
+                <h4>2024 Quiz</h4>
+                <h4>2023 Quiz</h4>
+                <h4>2022 Quiz</h4>
+            </div>
+            <div className="video">
+                <div className="video-main">
+                    <div className="img-info">
+                        <img src={infor} alt="" />
+                    </div>
+                    <div className="info-text">
+                        <h3>Watch our documentary on the Quiz Competition</h3>
+                        <p>Sigma Club</p>
+                    </div>
+                </div>
+
+                <div className="info-button">
+                    <button><IoPlayCircleOutline />Play Video</button>
+                </div>
+
+            </div>
+
+
+
+        </div>
+    )
+}
+export default Homepage
