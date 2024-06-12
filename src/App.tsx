4ec334449d106fbbadca7f07c3891662aad9b6b8
Silas <silasadegoke331@gmail.com>
2024-06-12 15:33:38 +0100

    completed the update form and the password confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
 import React from 'react';
+import "./App.css";
 import Homepage from './components/Homepage/Homepage';
 import { ChakraProvider } from '@chakra-ui/react'
 import { Routes, Route } from 'react-router-dom';
 import SelectQuiz from './components/Selectquiz/SelectQuiz';
 import About from './components/About/About';
+import AddSchool from './components/Addschool/AddSchool';
 import NotFound from './components/NotFound';
 import Login from './components/Registration/Login/Login';
 import Signin from './components/Registration/Signin/Signin';
-
+import NormalUsers from './components/NormalUsers/NormalUsers';
+import SchoolDetails from './components/NormalUsers/SchoolDetails/SchoolDetails';
+import TestDetails from './components/TestDetails/TestDetails';
+import AccountSettings from './components/account_settings/AccountSettings';
+import path from 'path';
+import PasswordSettings from './components/account_settings/password_settings/PasswordSettings';
+import ProfileSettings from './components/account_settings/profile_settings/ProfileSettings';
+import MainSubAdmin from './components/Subadmin/main/Main';
+import ManageUsers from './components/Subadmin/ManageUsers/ManageUsers';
+import UpdateUser from './components/Update user/UpdateUser';
+import UserProfile from './components/Update user Profile/UserProfile';
+import UserFunctions from './components/User Functions/UserFunctions';
 function App() {
   return (
-    <ChakraProvider>    
-      <div className="App">
-      
+    <ChakraProvider>
+      <div className="App">      
       <Routes>
         <Route element={<Homepage/>} path='/'/>
+        <Route element={<MainSubAdmin/>} path="/subadmin">
+            <Route index element={<ManageUsers/>}/>
+            <Route path='manage-users' element={<ManageUsers/>}/>
+              <Route path=':schools' element={<SchoolDetails/>}/>
 
+        </Route>
+        <Route element={<AddSchool quizName="2024 Roseline Etuokwu Quiz Competition" dateCreated="2024 - 05 - 30"/>}path="/Addschool"/>
+        <Route element={<NormalUsers/>} path="/users">
+                <Route path=':schools' element={<SchoolDetails/>}/>
+          </Route>
+        <Route element={<TestDetails/>} path="/users/test-details"/>
         <Route element={<SelectQuiz option='select' />} path='/select-quiz'/>
         <Route element={<SelectQuiz option='add' />} path='/add-quiz'/>
         <Route element={<SelectQuiz option='edit' />} path='/edit-quiz'/>
@@ -23,10 +45,12 @@ function App() {
         <Route element={<Login/>} path='/Login'/>
         <Route element={<Signin/>} path='/Signin'/>
         <Route element={<NotFound/>} path='*'/>
-
-      </Routes>
-      
-
+        <Route element={<AccountSettings />} path= {'/account-settings'}>
+          <Route path='password-settings' element={<PasswordSettings />} />
+          <Route path='profile-settings' element={<ProfileSettings />} />
+          <Route index element={<ProfileSettings />} />
+        </Route>
+      </Routes>     
     </div>
     </ChakraProvider>
   );
--- src/components/Selectquiz/SelectQuiz.tsx
+++ src/components/Selectquiz/SelectQuiz.tsx
@@ -10,46 +10,54 @@ import AddQuiz from '../Addquiz/AddQuiz';
 type selectQuizProps = {
   option: string
 }
+<<<<<<< Updated upstream
 
 const SelectQuiz = ({option} : selectQuizProps) => {
+=======
+const SelectQuiz = ({option} : selectQuizProps) => {
+  const navigate=useNavigate()
+>>>>>>> Stashed changes
   return (
-    <div className='select-quiz-page'>
-
+    <div className="select-quiz-page">
       <div className="select-quiz-container">
-
         <div className="logo">
           <img src={logo} alt="" />
         </div>
         <h3>Select Quiz</h3>
         <p>Select which quiz you choose to operate</p>
 
-        <div className='select-field'>
+        <div className="select-field">
           <select name="" id="">
             <option value="">2024 Roseline Etuokwu Quiz Competition</option>
           </select>
-          <IoIosArrowDown size={30} color='black' className='arrow-down' />
+          <IoIosArrowDown size={30} color="black" className="arrow-down" />
         </div>
 
         <div className="add-edit-btns">
-          <Link to='/add-quiz'>
-            <LuPlusCircle size={24} color='#555555' className='icons' />
+          <Link to="/add-quiz">
+            <LuPlusCircle size={24} color="#555555" className="icons" />
             Add Quiz
           </Link>
-          <Link to='/edit-quiz' className='icons'>
-            <GoPencil size={20} color='#555555' />
+          <Link to="/edit-quiz" className="icons">
+            <GoPencil size={20} color="#555555" />
             Edit Quiz
           </Link>
         </div>
 
         <div className="get-started-btn">
+<<<<<<< Updated upstream
           <button>Get Started</button>
         </div>
         
-        <div className="logout">
-          <Link to=''>Log Out</Link>
+=======
+          <button onClick={() => navigate("/AddSchool")}>Get Started</button>
         </div>
-
+>>>>>>> Stashed changes
+        <div className="logout">
+          <Link to="">Log Out</Link>
+        </div>
       </div>
+<<<<<<< Updated upstream
       
       {option === 'add' && 
       <>
@@ -62,8 +70,23 @@ const SelectQuiz = ({option} : selectQuizProps) => {
       <div className="modal-background"></div>
       </>}
 
+=======
+      {option === "add" && (
+        <>
+          <div className="modal-background"></div>
+          <AddQuiz />
+        </>
+      )}
+
+      {option === "edit" && (
+        <>
+          <div className="modal-background"></div>
+          <EditQuiz />
+        </>
+      )}
+>>>>>>> Stashed changes
     </div>
-  )
+  );
 }
 
 export default SelectQuiz
