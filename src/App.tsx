0103931071892a1778bf52b7ad1518df415d8ebf
Abasir <adiadebayo1@gmail.com>
2024-06-12 13:24:38 +0100

    addschool to quiz section

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
+import "./App.css";
 import Homepage from './components/Homepage/Homepage';
 import { ChakraProvider } from '@chakra-ui/react'
 import { Routes, Route } from 'react-router-dom';
 import SelectQuiz from './components/Selectquiz/SelectQuiz';
 import About from './components/About/About';
+import AddSchool from './components/Addschool/AddSchool';
 import NotFound from './components/NotFound';
 import Login from './components/Registration/Login/Login';
 import Signin from './components/Registration/Signin/Signin';
 
 function App() {
   return (
-    <ChakraProvider>    
+    <ChakraProvider>
       <div className="App">
-      
-      <Routes>
-        <Route element={<Homepage/>} path='/'/>
+        <Routes>
+          <Route element={<Homepage />} path="/" />
 
-        <Route element={<SelectQuiz option='select' />} path='/select-quiz'/>
-        <Route element={<SelectQuiz option='add' />} path='/add-quiz'/>
-        <Route element={<SelectQuiz option='edit' />} path='/edit-quiz'/>
-        <Route element={<About/>} path='/About'/>
-        <Route element={<Login/>} path='/Login'/>
-        <Route element={<Signin/>} path='/Signin'/>
-        <Route element={<NotFound/>} path='*'/>
-
-      </Routes>
-      
-
-    </div>
+          <Route element={<SelectQuiz option="select" />} path="/select-quiz" />
+          <Route element={<SelectQuiz option="add" />} path="/add-quiz" />
+          <Route element={<SelectQuiz option="edit" />} path="/edit-quiz" />
+          <Route element={<AddSchool quizName="2024 Roseline Etuokwu Quiz Competition" dateCreated="2024 - 05 - 30"/>}path="/Addschool"/>
+          <Route element={<About />} path="/About" />
+          <Route element={<Login />} path="/Login" />
+          <Route element={<Signin />} path="/Signin" />
+          <Route element={<NotFound />} path="*" />
+        </Routes>
+      </div>
     </ChakraProvider>
   );
 }
